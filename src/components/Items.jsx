68b462a67e23d9a8f6a91bee31013c7373d0758f
12345120 <hehe9343@gmail.com>
2022-01-03 22:46:52 -0500

    finished version of Item-based scrollmagic
    The feature_forwardRef (the other branch) is a different attempt at using forwardRef to use scrollmagic at a higer scope.

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -6,41 +6,38 @@ import "../css/util.css";
 function Items() {
   return (
     <div className="Items">
-      <Controller>
-        <Item
-          color={"#DC4D2E"}
-          text={"red circle"}
-          id_num={1}
-          slide_direction={"vert"}
-          slide_offset={"100px"}
-        />
-        <Item
-          color={"#2E8ADC"}
-          text={"blue circle"}
-          id_num={2}
-          slide_direction={"horiz"}
-          slide_offset={"100px"}
-        />
-        <Item
-          color={"#DCDA2E"}
-          text={"yellow circle"}
-          id_num={3}
-          slide_direction={"horiz"}
-          slide_offset={"100px"}
-        />
-      </Controller>
+      <Item
+        color={"#DC4D2E"}
+        text={"red circle"}
+        id_num={1}
+        slide_direction={"vert"}
+        slide_offset={"100px"}
+      />
+      <Item
+        color={"#2E8ADC"}
+        text={"blue circle"}
+        id_num={2}
+        slide_direction={"horiz"}
+        slide_offset={"100px"}
+      />
+      <Item
+        color={"#DCDA2E"}
+        text={"yellow circle"}
+        id_num={3}
+        slide_direction={"horiz"}
+        slide_offset={"-100px"}
+      />
     </div>
   );
 }
 
-forwardRef();
 function Item({ color, text, id_num, slide_direction, slide_offset }) {
   const slide_translate_val = `translate${
-    slide_direction == "horiz" ? "X" : "Z"
+    slide_direction == "horiz" ? "X" : "Y"
   }(${slide_offset})`;
 
   const slide_style = {
-    opacity: "0",
+    opacity: "0.5",
     transform: slide_translate_val,
   };
 
@@ -48,14 +45,15 @@ function Item({ color, text, id_num, slide_direction, slide_offset }) {
     <Controller>
       <Scene
         triggerHook={0.7}
-        triggerElement={"#circle-1"}
-        classTogle={""}
-        reverse={false}
+        triggerElement={`#circle-${id_num}`}
+        offset={`${slide_direction === "horiz" ? "0" : slide_offset.substring(slide_offset.length-2)}`}
+        classToggle={[`.circle-${id_num}`, `${slide_direction === "horiz" ? "slide-horiz" : "slide-vert"}`]}
+        reverse={true}
         indicators={true}
       >
         <div
-          className="Item"
-          style={{ backgroundColor: color, ...slide_style }}
+          className={`Item circle-${id_num}`}
+          style={{ backgroundColor: color, ...slide_style }} 
           id={`circle-${id_num}`}
         >
           <div className="Item_text">{text}</div>
