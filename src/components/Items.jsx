eb1da82163f7da2667e5b660577fbc1f5ef152fe
12345120 <hehe9343@gmail.com>
2022-01-03 20:20:58 -0500

    added basic scrollmagic code

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -1,20 +1,67 @@
+import { forwardRef } from "react";
+import { Controller, Scene } from "react-scrollmagic";
 import "../css/Items.css";
+import "../css/util.css";
 
 function Items() {
   return (
     <div className="Items">
-      <Item color={"#DC4D2E"} text={"red circle"} />
-      <Item color={"#2E8ADC"} text={"blue circle"} />
-      <Item color={"#DCDA2E"} text={"yellow circle"} />
+      <Controller>
+        <Item
+          color={"#DC4D2E"}
+          text={"red circle"}
+          id_num={1}
+          slide_direction={"vert"}
+          slide_offset={"100px"}
+        />
+        <Item
+          color={"#2E8ADC"}
+          text={"blue circle"}
+          id_num={2}
+          slide_direction={"horiz"}
+          slide_offset={"100px"}
+        />
+        <Item
+          color={"#DCDA2E"}
+          text={"yellow circle"}
+          id_num={3}
+          slide_direction={"horiz"}
+          slide_offset={"100px"}
+        />
+      </Controller>
     </div>
   );
 }
 
-function Item({ color, text }) {
+forwardRef();
+function Item({ color, text, id_num, slide_direction, slide_offset }) {
+  const slide_translate_val = `translate${
+    slide_direction == "horiz" ? "X" : "Z"
+  }(${slide_offset})`;
+
+  const slide_style = {
+    opacity: "0",
+    transform: slide_translate_val,
+  };
+
   return (
-    <div className="Item" style={{ backgroundColor: color }}>
-      <div className="Item_text">{text}</div>
-    </div>
+    <Controller>
+      <Scene
+        triggerHook={0.7}
+        triggerElement={"#circle-1"}
+        classTogle={""}
+        reverse={false}
+        indicators={true}
+      >
+        <div
+          className="Item"
+          style={{ backgroundColor: color, ...slide_style }}
+          id={`circle-${id_num}`}
+        >
+          <div className="Item_text">{text}</div>
+        </div>
+      </Scene>
+    </Controller>
   );
 }
 
